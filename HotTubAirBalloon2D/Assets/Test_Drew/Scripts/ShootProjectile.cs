81c16da569e9012282b868e3175674a1530abed6
Drew Graham <Drew Graham>
2019-01-20 14:40:05 -0500

    projectiles now move in direction of mouse click at fixed speed

---
+++ HotTubAirBalloon2D/Assets/Test_Drew/Scripts/ShootProjectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour
+{
+
+    public Projectile projectile;
+
+    public float maxSpeed;
+
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // Get direction of click
+            Vector2 direction = mousePos - (Vector2)transform.position;
+
+            // Clamp to max speed
+            Vector2 trajectory = Vector3.Normalize(direction) * maxSpeed;
+
+            Debug.Log("Shooting projectile towards " + trajectory + " at speed " + trajectory.magnitude);
+            Shoot(trajectory);
+        }
+    }
+
+
+    void Shoot(Vector2 trajectory)
+    {
+        Projectile proj = GameObject.Instantiate(projectile, transform.position, transform.rotation);
+        proj.SetDirection(trajectory);
+    }
+}
