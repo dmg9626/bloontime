4b84cdc701ff21bc113de11509a4b1783b53f0cd
callmeshua <callmeshua620@gmail.com>
2019-02-01 13:08:24 -0500

    Prod Scene

---
+++ HotTubAirBalloon2D/Assets/Production/Scripts/ClickBurst.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBurst : MonoBehaviour
+{
+    ParticleSystem psystem;
+
+    public int particleCount;
+
+    public GameObject trigger;
+
+    public Gradient fireGradient;
+
+    public Gradient iceGradient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        psystem = GetComponent<ParticleSystem>();
+    }
+
+    public void Burst(Vector2 position)
+    {
+        psystem.transform.position = position;
+        psystem.Emit(particleCount);
+        Instantiate(trigger, position, transform.rotation);
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            SetColorOverLifetime(fireGradient);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            SetColorOverLifetime(iceGradient);
+        }
+    }
+
+    void SetColorOverLifetime(Gradient gradient)
+    {
+        var colorModule = psystem.colorOverLifetime;
+        colorModule.color = gradient;
+    }
+}
---
+++ HotTubAirBalloon2D/Assets/Production/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    Vector3 velocity;
+
+    Rigidbody2D rb;
+
+    public void SetDirection(Vector2 direction) 
+    {
+        velocity = direction;
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+    }
+}
