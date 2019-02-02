e1fbd72d00ef93311a5507e7410ecb43dac21f51
Drew Graham <dmg9626@gmail.com>
2019-02-01 19:20:47 -0500

    reworked Player prefab into Ballon prefab containing 2 players with independently controlled cursors (still attack on same button)

--- HotTubAirBalloon2D/Assets/Production/Scripts/Player/AttackManager.cs
+++ HotTubAirBalloon2D/Assets/Production/Scripts/Player/AttackManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandleClick : MonoBehaviour
+public class AttackManager : MonoBehaviour
 {
     public enum ShootMode {
         MOVING_PROJECTILE,
