f93e309bd14cb06893a7ad0757ee5c9b72df1b79
Drew Graham <Drew Graham>
2019-02-05 23:01:47 -0500

    added responsive color bg to temperature/comfort meters (thermometer-like appearance)

---
+++ HotTubAirBalloon2D/Assets/Production/Scripts/UI/SliderUpdate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderUpdate : MonoBehaviour
+{
+    public Gradient gradient;
+
+    public Slider slider;
+
+    public Image sliderBG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Slider value start: " + GetSliderValue01(slider.value));
+        sliderBG.color = gradient.Evaluate(GetSliderValue01(slider.value));
+
+        slider.onValueChanged.AddListener(SetSliderColor);
+    }
+
+    float GetSliderValue01(float value)
+    {
+        float value01 = value / Mathf.Abs(slider.maxValue - slider.minValue);
+        return value01;
+    }
+
+    public void SetSliderColor(float value)
+    {
+        sliderBG.color = gradient.Evaluate(GetSliderValue01(slider.value));
+    }
+}
