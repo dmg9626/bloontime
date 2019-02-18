1ebdd5dbd6a432a5a84673e5a5df03aa638d8d98
Drew Graham <Drew Graham>
2019-02-17 20:03:50 -0500

    clamping cursor position to screen bounds

--- HotTubAirBalloon2D/Assets/Production/Scripts/Player/CursorMovement.cs
+++ HotTubAirBalloon2D/Assets/Production/Scripts/Player/CursorMovement.cs
@@ -13,52 +13,46 @@ public class CursorMovement : MonoBehaviour
 
     public Transform cursor;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if (playerNum.Equals(PlayerNumber.ONE))
-        {
-            PlayerOneUpdate();
-        }
-        else if (playerNum.Equals(PlayerNumber.TWO))
-        {
-            PlayerTwoUpdate();
-        }
+        UpdateCursor(playerNum);
     }
 
-    void PlayerOneUpdate()
+    void UpdateCursor(PlayerNumber playerNumber)
     {
         Vector2 pos = cursor.position;
-        if (Input.GetKey(KeyCode.A))
-            pos.x -= moveSpeed;
-        if (Input.GetKey(KeyCode.D))
-            pos.x += moveSpeed;
-        if (Input.GetKey(KeyCode.W))
-            pos.y += moveSpeed;
-        if (Input.GetKey(KeyCode.S))
-            pos.y -= moveSpeed;
 
-        cursor.position = pos;
-    }
+        // Handle player one inputs
+        if(playerNumber.Equals(PlayerNumber.ONE)) {
+            if (Input.GetKey(KeyCode.A))
+                pos.x -= moveSpeed;
+            if (Input.GetKey(KeyCode.D))
+                pos.x += moveSpeed;
+            if (Input.GetKey(KeyCode.W))
+                pos.y += moveSpeed;
+            if (Input.GetKey(KeyCode.S))
+                pos.y -= moveSpeed;
+        }
+        // Handle player two inputs
+        else if(playerNumber.Equals(PlayerNumber.TWO)) {
+            if (Input.GetKey(KeyCode.LeftArrow))
+                pos.x -= moveSpeed;
+            if (Input.GetKey(KeyCode.RightArrow))
+                pos.x += moveSpeed;
+            if (Input.GetKey(KeyCode.UpArrow))
+                pos.y += moveSpeed;
+            if (Input.GetKey(KeyCode.DownArrow))
+                pos.y -= moveSpeed;
+        }
 
-    void PlayerTwoUpdate()
-    {
-        Vector2 pos = cursor.position;
-        if (Input.GetKey(KeyCode.LeftArrow))
-            pos.x -= moveSpeed;
-        if (Input.GetKey(KeyCode.RightArrow))
-            pos.x += moveSpeed;
-        if (Input.GetKey(KeyCode.UpArrow))
-            pos.y += moveSpeed;
-        if (Input.GetKey(KeyCode.DownArrow))
-            pos.y -= moveSpeed;
+        // Get cursor position in screen space (where 0,0 = min corner, 1,1 = max corner)
+        Vector2 screenPos = Camera.main.WorldToViewportPoint(pos);
 
-        cursor.position = pos;
+        // Clamp position to screen bounds
+        screenPos = new Vector2(Mathf.Clamp01(screenPos.x), Mathf.Clamp01(screenPos.y));
+
+        // Convert properly-clamped screen position back to world space
+        cursor.position = (Vector2)Camera.main.ViewportToWorldPoint(screenPos);
     }
 }
