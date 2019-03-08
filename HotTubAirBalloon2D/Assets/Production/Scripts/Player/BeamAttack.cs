420d933e205bd654ca7cece26f898848265b9065
Drew Graham <Drew Graham>
2019-03-07 23:10:26 -0500

    beam attack now works more smoothly, follows cursor, only triggers on button press

--- HotTubAirBalloon2D/Assets/Production/Scripts/Player/BeamAttack.cs
+++ HotTubAirBalloon2D/Assets/Production/Scripts/Player/BeamAttack.cs
@@ -4,28 +4,72 @@ using UnityEngine;
 
 public class BeamAttack : MonoBehaviour
 {
-    public Transform target;
+    /// <summary>
+    /// Position of cursor
+    /// </summary>
+    public Transform cursor;
 
-    public float speed;
+    /// <summary>
+    /// Max speed for beam to rotate towards cursor
+    /// </summary>
+    public float rotationSpeed;
 
-    Transform init;
+    /// <summary>
+    /// Max beam expansion speed
+    /// </summary>
+    // public float expansionSpeed;
 
-    // Update is called once per frame
+    BoxCollider2D col;
+
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+    }
+
     void Update()
     {
+        if(Input.GetKey(KeyCode.Space)) {
+            FireBeam();
+        }
+        else {
+             transform.localScale = Vector3.one;
+             RotateTowardsCursor();
+        }
+    }
+
+    void RotateTowardsCursor()
+    {
         // Get angle to target (in degrees)
-        Vector3 vectorToTarget = target.position - transform.position;
-        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Vector3 vectorToCursor = cursor.position - transform.position;
+        float angle = Mathf.Atan2(vectorToCursor.y, vectorToCursor.x) * Mathf.Rad2Deg;
 
-        // Debug.Log("Angle to target: " + angle);
-
+        // Calculate rotation towards cursor
         Quaternion q = Quaternion.AngleAxis(angle - 90, Vector3.forward);
 
-        Debug.LogWarning(q.eulerAngles);
-        Debug.Log(transform.rotation.eulerAngles);
+        // Smooth rotation towards cursor
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, Time.deltaTime * rotationSpeed);
+    }
 
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, Time.deltaTime * speed);
-        // transform.Rotate(Vector3.forward, angle - 90, Space.Self);
-        transform.localScale = new Vector3(1, vectorToTarget.magnitude, 1);
+    // Update is called once per frame
+    void FireBeam()
+    {
+        RotateTowardsCursor();
+        
+        // Calculate vector towards cursor
+        Vector3 vectorToCursor = cursor.position - transform.position;
+
+        // Distance to cursor
+        float distanceToCursor = vectorToCursor.magnitude;
+
+        // Get beam length (scaled between 0 and 1)
+        float beamLengthScaled = transform.localScale.y / distanceToCursor;
+
+        // Scale beam length towards distanceToCursor
+        float beamLength = Mathf.Lerp(1, distanceToCursor, beamLengthScaled);
+        transform.localScale = new Vector3(1, beamLength, 1);
+
+        // Debug.Log("Beam length: " + beamLength);
+        // Debug.Log("Beam length scaled: " + beamLengthScaled);
+        // Debug.Log("Distance: " + distanceToCursor);
     }
 }
