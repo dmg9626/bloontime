00423225bf30f5732e29377595b30c9dc42e4a1a
Drew Graham <dmg9626@gmail.com>
2019-03-10 16:11:04 -0400

    beam attack now works! just need to integrate into main scene and make it pretty

--- HotTubAirBalloon2D/Assets/Production/Scripts/Player/BeamAttack.cs
+++ HotTubAirBalloon2D/Assets/Production/Scripts/Player/BeamAttack.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class BeamAttack : MonoBehaviour
 {
-    /// <summary>
-    /// Position of cursor
-    /// </summary>
     public Transform cursor;
+
+    public ClickBurst.EffectType effectType;
+
+    public float damagePerSecond;
 
     /// <summary>
     /// Max speed for beam to rotate towards cursor
@@ -19,16 +20,10 @@ public class BeamAttack : MonoBehaviour
     /// </summary>
     public float expansionSpeed;
 
-
     /// <summary>
     /// Interpolation value used for beam growth in FireBeam()
     /// </summary>
     private float beamInterpolationValue;
-
-    /// <summary>
-    /// Max beam expansion speed
-    /// </summary>
-    // public float expansionSpeed;
 
     BoxCollider2D col;
 
@@ -42,10 +37,12 @@ public class BeamAttack : MonoBehaviour
     {
         RotateTowardsCursor();
 
-        if (Input.GetKey(KeyCode.Space)) {
+        if (Input.GetKey(KeyCode.Space))
+        {
             FireBeam();
         }
-        else {
+        else
+        {
             transform.localScale = Vector3.one;
             beamInterpolationValue = 0;
         }
@@ -61,7 +58,7 @@ public class BeamAttack : MonoBehaviour
         Quaternion q = Quaternion.AngleAxis(angle - 90, Vector3.forward);
 
         // Smooth rotation towards cursor
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, Time.deltaTime * rotationSpeed);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, Time.fixedDeltaTime * rotationSpeed);
     }
 
     // Update is called once per frame
@@ -72,10 +69,30 @@ public class BeamAttack : MonoBehaviour
         float distanceToCursor = vectorToCursor.magnitude;
 
         // Get time value to interpolate over beam growth curve
-        beamInterpolationValue = Mathf.Clamp01(beamInterpolationValue + (Time.deltaTime * expansionSpeed));
+        beamInterpolationValue = Mathf.Clamp01(beamInterpolationValue + (Time.fixedDeltaTime * expansionSpeed));
 
         // Scale beam length towards distanceToCursor
         float beamLength = Mathfx.Sinerp(1, distanceToCursor, beamInterpolationValue);
         transform.localScale = new Vector3(1, beamLength, 1);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        GameObject obj = collision.gameObject;
+        if (LayerMask.LayerToName(obj.layer) == "Vulnerable" || LayerMask.LayerToName(obj.layer) == "Player")
+        {
+            // Damage enemy
+            if (collision.tag == "Enemy" && collision.GetComponent<AbstractObstacle>() != null)
+            {
+                Debug.Log(name + " collided with " + obj.name);
+                collision.GetComponent<AbstractObstacle>().takeDamage(effectType, damagePerSecond * Time.fixedDeltaTime);
+            }
+
+            // Destroy projectile
+            if (collision.tag == "Projectile" && collision.GetComponent<AbstractProjectile>() != null)
+            {
+                collision.GetComponent<AbstractProjectile>().takeDamage(effectType);
+            }
+        }
     }
 }
