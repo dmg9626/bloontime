1463fd61842b3b82b16277c409c0f843c684429c
Drew Graham <Drew Graham>
2019-04-14 14:42:08 -0400

    main menu now functional (play button loads game scene, still need to implement quit game button

---
+++ HotTubAirBalloon2D/Assets/Production/Scripts/Managers/LoadSceneButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    public enum Scene {
+        MAIN_MENU,
+        GAME
+    }
+
+    public Scene sceneToOpen;
+
+    public Dictionary<Scene, string> sceneDict = new Dictionary<Scene, string>() {
+        {Scene.MAIN_MENU, "MainMenu"},
+        {Scene.GAME, "ProcGenTest"}
+    };
+
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(Load);
+    }
+    
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneDict[sceneToOpen]);
+    }
+}
